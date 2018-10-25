427098cfb1c80fc6a8daaff646a4f4b5eb348365
hzfanjinlong <hzfanjinlong@corp.netease.com>
2018-10-25 21:02:49 +0800

    zk 学习

---
+++ src/main/java/com/maxam/zk/ZooKeeperServerStarter.java
@@ -0,0 +1,17 @@
+package com.maxam.zk;
+
+import org.apache.zookeeper.server.quorum.QuorumPeerMain;
+
+/**
+ * @author fanjinlong
+ * @date 2018-10-23 15:51
+ **/
+public class ZooKeeperServerStarter {
+
+
+	public static void main(String[] args) {
+		String str = "C:\\Users\\hzfanjinlong\\IdeaProjects\\springlearning\\zookeeper\\src\\main\\resources\\zoo2.cfg";
+		String [] arg1 = new String[]{str};
+		QuorumPeerMain.main(arg1);
+	}
+}
